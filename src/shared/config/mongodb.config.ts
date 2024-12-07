8665421838769087d889ee3d1c254f90d1dbc8b2
Johannes Schäfer <johannes.ludwig.schaefer@gmail.com>
2024-12-07 15:09:58 +0100

    works now

--- src/shared/config/mongodb.config.ts
+++ src/shared/config/mongodb.config.ts
@@ -7,10 +7,12 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
  */
 export default registerAs('mongodb', () => {
 	const { MONGO_DB_PORT, MONGO_DB_HOSTNAME, MONGO_DB_DATABASE, MONGO_DB_USERNAME, MONGO_DB_PASSWORD } = process.env;
-
+	console.log(
+		`mongodb://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOSTNAME}:${MONGO_DB_PORT}/${MONGO_DB_DATABASE}`,
+	);
 	return {
 		uri: isDevelopment
 			? `mongodb://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOSTNAME}:${MONGO_DB_PORT}/${MONGO_DB_DATABASE}`
-			: `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOSTNAME}/${MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+			: `mongodb+srv://${MONGO_DB_USERNAME}:${MONGO_DB_PASSWORD}@${MONGO_DB_HOSTNAME}/${MONGO_DB_DATABASE}?retryWrites=true&w=majority&authSource=admin`,
 	};
 });
