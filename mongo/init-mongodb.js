2a93b4471c4b08750bc6c988f599597943c7ee88
Johannes Schäfer <johannes.ludwig.schaefer@gmail.com>
2024-12-11 10:39:37 +0100

    update mongo init

--- mongo/init-mongodb.js
+++ mongo/init-mongodb.js
@@ -1,19 +1,11 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 db.createUser({
 	user: process.env.MONGO_DB_USERNAME,
-	pwd: passwordPrompt(),
+	pwd: process.env.MONGO_DB_PASSWORD,
 	roles: [
 		{
 			role: 'dbAdmin',
 			db: process.env.MONGO_DB_DATABASE,
 		},
 	],
-});
-db.createCollection('users');
-const hashedPassword = bcrypt.hashSync('admin', 10);
-db.users.insertOne({
-	firstName: 'admin',
-	lastName: 'admin',
-	email: 'admin@admin.com',
-	password: hashedPassword, // Store the hashed password
 });
