87b6874dce601177e3b78b1973e16ab52372867e
Johannes Schäfer <johannes.ludwig.schaefer@gmail.com>
2024-12-16 21:48:06 +0100

    testing github actions

--- mongo/init-mongodb.js
+++ mongo/init-mongodb.js
@@ -1,19 +1,16 @@
+// mongo/init-mongodb.js
 const bcrypt = require('bcrypt');
+
 db.createUser({
 	user: process.env.MONGO_DB_USERNAME,
-	pwd: passwordPrompt(),
-	roles: [
-		{
-			role: 'dbAdmin',
-			db: process.env.MONGO_DB_DATABASE,
-		},
-	],
+	pwd: process.env.MONGO_DB_PASSWORD,
+	roles: [{ role: 'readWrite', db: process.env.MONGO_DB_DATABASE }],
 });
-db.createCollection('users');
-const hashedPassword = bcrypt.hashSync('admin', 10);
-db.users.insertOne({
-	firstName: 'admin',
-	lastName: 'admin',
-	email: 'admin@admin.com',
-	password: hashedPassword, // Store the hashed password
-});
+// db.createCollection('users');
+// const hashedPassword = bcrypt.hashSync('admin', 10);
+// db.users.insertOne({
+// 	firstName: 'admin',
+// 	lastName: 'admin',
+// 	email: 'admin@admin.com',
+// 	password: hashedPassword, // Store the hashed password
+// });
