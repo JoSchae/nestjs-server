f8613cf58797223baa809ed08e3e4d40be8a8e22
Johannes Schäfer <johannes.ludwig.schaefer@gmail.com>
2024-12-17 18:26:29 +0100

    dbadmin?

--- mongo/init-mongodb.js
+++ mongo/init-mongodb.js
@@ -1,16 +1,22 @@
 // mongo/init-mongodb.js
 const bcrypt = require('bcrypt');
 
+db.use('auth');
+db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
+// creates a db user
 db.createUser({
 	user: process.env.MONGO_DB_USERNAME,
 	pwd: process.env.MONGO_DB_PASSWORD,
-	roles: [{ role: 'readWrite', db: process.env.MONGO_DB_DATABASE }],
+	roles: [{ role: 'dbAdmin', db: process.env.MONGO_DB_DATABASE }],
 });
+db.use('process.env.MONGO_DB_DATABASE');
+db.auth(process.env.MONGO_DB_USERNAME, process.env.MONGO_DB_PASSWORD);
 db.createCollection('users');
 const hashedPassword = bcrypt.hashSync(process.env.MONGO_DB_ADMINUSER_PASSWORD, 10);
+// creates a nestjs user
 db.users.insertOne({
 	firstName: 'admin',
 	lastName: 'admin',
-	email: 'admin@admin.com',
+	email: process.env.MONGO_DB_ADMINUSER_PASSWORD,
 	password: hashedPassword, // Store the hashed password
 });
