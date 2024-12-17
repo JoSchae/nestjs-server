c90e92c759c5e7fc7ec51ccf38a40a36a844342f
Johannes Schäfer <johannes.ludwig.schaefer@gmail.com>
2024-12-17 18:50:42 +0100

    update mongodb config to use my database

--- mongo/init-mongodb.js
+++ mongo/init-mongodb.js
@@ -1,22 +1,25 @@
 // mongo/init-mongodb.js
 const bcrypt = require('bcrypt');
 
-db.use('auth');
+db = db.getSiblingDB('admin');
 db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
-// creates a db user
+
+db = db.getSiblingDB(process.env.MONGO_DB_DATABASE);
+
+// Create application user
 db.createUser({
 	user: process.env.MONGO_DB_USERNAME,
 	pwd: process.env.MONGO_DB_PASSWORD,
-	roles: [{ role: 'dbAdmin', db: process.env.MONGO_DB_DATABASE }],
+	roles: [{ role: 'readWrite', db: process.env.MONGO_DB_DATABASE }],
 });
-db.use('process.env.MONGO_DB_DATABASE');
-db.auth(process.env.MONGO_DB_USERNAME, process.env.MONGO_DB_PASSWORD);
+
+// Create collections and initial data
 db.createCollection('users');
 const hashedPassword = bcrypt.hashSync(process.env.MONGO_DB_ADMINUSER_PASSWORD, 10);
-// creates a nestjs user
+
 db.users.insertOne({
 	firstName: 'admin',
 	lastName: 'admin',
-	email: process.env.MONGO_DB_ADMINUSER_PASSWORD,
-	password: hashedPassword, // Store the hashed password
+	email: process.env.MONGO_DB_ADMIN_USERNAME,
+	password: hashedPassword,
 });
